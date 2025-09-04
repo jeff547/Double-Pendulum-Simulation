a1ecaae5de032d9702e5f2c96e95632c2925a4cb
jeff547 <jefflu547@gmail.com>
2025-09-04 15:30:21 -0400

    Added Cmake.

---
+++ out/build/main/CMakeFiles/Double_Pendulum_Simulation.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Double_Pendulum_Simulation.
+# This may be replaced when dependencies are built.
---
+++ out/build/main/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation
+# Build directory: /Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/out/build/main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
