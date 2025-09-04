00ea8ede7c3dbda1c411d0721ad45ad5216e4a4a
jeff547 <jefflu547@gmail.com>
2025-09-04 16:28:29 -0400

    Optimized Cmakelist.txt

---
+++ out/build/default/CMakeFiles/Double_Pendulum_Simulation.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/glad.c" "CMakeFiles/Double_Pendulum_Simulation.dir/glad.c.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/glad.c.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/Ball.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/Ball.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/Ball.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/DoublePendulum.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulum.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulum.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/DoublePendulumODE.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulumODE.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulumODE.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/Lab.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/Lab.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/Lab.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/RigidBody.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/RigidBody.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/RigidBody.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/Rod.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/Rod.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/Rod.cpp.o.d"
+  "/Users/jeff547/Developer/Computer Graphics/Double Pendulum Simulation/main.cpp" "CMakeFiles/Double_Pendulum_Simulation.dir/main.cpp.o" "gcc" "CMakeFiles/Double_Pendulum_Simulation.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/build/default/CMakeFiles/Double_Pendulum_Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Ball.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Ball.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulum.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulum.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulumODE.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/DoublePendulumODE.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Lab.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Lab.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/RigidBody.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/RigidBody.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Rod.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/Rod.cpp.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/glad.c.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/glad.c.o.d"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/main.cpp.o"
+  "CMakeFiles/Double_Pendulum_Simulation.dir/main.cpp.o.d"
+  "Double_Pendulum_Simulation"
+  "Double_Pendulum_Simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Double_Pendulum_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
